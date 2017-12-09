da59c345119d0954a8aaf7e29b2eea0f449aa668
Konrad Ociepka <ociepkakonrad@gmail.com>
2017-12-09 12:48:24 +0100

    Add books method to Category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,8 @@
 class Category < ActiveYaml::Base
   extend ActiveHash::Associations::ActiveRecordExtensions
   set_root_path "db/data"
+
+  def books
+    ::Book.where(category_id: self.id)
+  end
 end
